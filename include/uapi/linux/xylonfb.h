06903357eef169ad98783bb1006e4560bfa7b377
Leigh Cameron <leigh.cameron@tacticalresearch.com.au>
2015-10-09 11:51:55 +1100

    SHARP-45 uapi header for the custom xylonfb ioctls.

--- include/uapi/linux/xylonfb.h
+++ include/uapi/linux/xylonfb.h
@@ -19,6 +19,18 @@
 
 #include <linux/types.h>
 
+struct xylonfb_hw_access {
+	__u32 offset;
+	__u32 value;
+	bool set;
+};
+
+/* Used only with logiCVC 3.x */
+struct xylonfb_layer_buffer {
+	__u8 id;
+	bool set;
+};
+
 struct xylonfb_layer_color {
 	__u32 raw_rgb;
 	__u8 use_raw;
@@ -33,6 +45,8 @@ struct xylonfb_layer_geometry {
 	__u16 y;
 	__u16 width;
 	__u16 height;
+	__u16 x_offset;
+	__u16 y_offset;
 	bool set;
 };
 
@@ -41,38 +55,31 @@ struct xylonfb_layer_transparency {
 	bool set;
 };
 
-struct xylonfb_hw_access {
-	__u32 offset;
-	__u32 value;
-	bool set;
-};
-
-/* Xylon FB events */
-#define XYLONFB_EVENT_FBI_UPDATE (1 << 0)
-
 /* Xylon FB IOCTL's */
 #define XYLONFB_IOW(num, dtype)		_IOW('x', num, dtype)
 #define XYLONFB_IOR(num, dtype)		_IOR('x', num, dtype)
 #define XYLONFB_IOWR(num, dtype)	_IOWR('x', num, dtype)
 #define XYLONFB_IO(num)			_IO('x', num)
 
-#define XYLONFB_VSYNC_CTRL		XYLONFB_IOR(30, unsigned char)
+#define XYLONFB_VSYNC_CTRL		XYLONFB_IOR(30, bool)
 #define XYLONFB_LAYER_IDX		XYLONFB_IOR(31, unsigned int)
 #define XYLONFB_LAYER_ALPHA		\
 	XYLONFB_IOR(32, struct xylonfb_layer_transparency)
-#define XYLONFB_LAYER_COLOR_TRANSP_CTRL	XYLONFB_IOW(33, unsigned char)
+#define XYLONFB_LAYER_COLOR_TRANSP_CTRL	XYLONFB_IOW(33, bool)
 #define XYLONFB_LAYER_COLOR_TRANSP \
 	XYLONFB_IOR(34, struct xylonfb_layer_color)
 #define XYLONFB_LAYER_GEOMETRY \
 	XYLONFB_IOR(35, struct xylonfb_layer_geometry)
-#define XYLONFB_LAYER_BUFFER_OFFSET	XYLONFB_IOR(36, unsigned int)
-#define XYLONFB_LAYER_BUFFER_SWITCH	XYLONFB_IOR(42, unsigned int)
+#define XYLONFB_LAYER_BUFFER \
+	XYLONFB_IOR(36, struct xylonfb_layer_buffer)
+#define XYLONFB_LAYER_BUFFER_OFFSET	XYLONFB_IOR(37, unsigned int)
 #define XYLONFB_BACKGROUND_COLOR \
-	XYLONFB_IOR(37, struct xylonfb_layer_color)
-#define XYLONFB_LAYER_EXT_BUFF_SWITCH	XYLONFB_IOW(38, unsigned char)
+	XYLONFB_IOR(38, struct xylonfb_layer_color)
+#define XYLONFB_LAYER_EXT_BUFF_SWITCH	XYLONFB_IOW(39, bool)
 #define XYLONFB_HW_ACCESS \
-	XYLONFB_IOR(39, struct xylonfb_hw_access)
-#define XYLONFB_WAIT_EDID		XYLONFB_IOW(40, unsigned int)
-#define XYLONFB_GET_EDID		XYLONFB_IOR(41, char)
+	XYLONFB_IOR(40, struct xylonfb_hw_access)
+#define XYLONFB_IP_CORE_VERSION		XYLONFB_IOR(41, __u32)
+#define XYLONFB_WAIT_EDID		XYLONFB_IOW(42, unsigned int)
+#define XYLONFB_GET_EDID		XYLONFB_IOR(43, char)
 
 #endif /* __XYLONFB_H__ */
